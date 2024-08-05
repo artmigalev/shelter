1b58889ed80008a07d67b0635cb3ab294bd8af08
artmigalev <art_migalev@yahoo.com>
2024-08-05 18:57:33 +0300

    refactor: refactored code

--- shelter/pages/main/index.js
+++ shelter/pages/main/index.js
@@ -1,14 +1,18 @@
-import pets from "../../assets/data/pets.json" with {"type":"json"};
-console.log(
-  "Страница Main (60):\n1.Проверка верстки +7\n2.Вёрстка соответствует макету +35\n3.Требования к css +6\n4.Интерактивность элементов +12\n  TOTAL SCORE: 100"
-);
+const pets = await fetch("../../assets/data/pets.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then(function (data) {
+    const dta = data;
+    return dta;
+  })
+  .catch((error) => console.log(error));
+console.log(pets);
 
 //*--------VARIABLES---- *//
 const BTN_BURGER = document.querySelector(".burger__menu");
 const navigation = document.querySelector(".header__navigation");
 const listNav = document.querySelector(".nav__list");
-
-BTN_BURGER.style.display = "flex";
 
 function check() {
   if (window.matchMedia("(min-width: 767px)").matches) {
@@ -34,98 +38,68 @@ function clickedBurger() {
 check();
 window.addEventListener("resize", check);
 BTN_BURGER.addEventListener("click", clickedBurger);
-const petsObj = pets[0];
 
+//***************************************************/
 
-
-//   console.log(petsObj);
-//   let popup = document.createElement('dialog')
-//   popup.className = 'popup-main'
-//   let btnClosePopup = document.createElement('button')
-//   btnClosePopup.className = 'button-slider'
-//   popup.appendChild(btnClosePopup)
-//   let popupContainer = document.createElement('div')
-//   popupContainer.className = 'popup-container'
-//   popup.appendChild(popupContainer)
-//   let imgCard = document.createElement('img')
-//   imgCard.src = petsObj.img
-//   imgCard.alt = petsObj.name.toLowerCase()
-//   popupContainer.appendChild(imgCard)
-
-//   let contentContainer = document.createElement('div')
-//   contentContainer.className = 'content-container'
-//   contentContainer.innerHTML =`
-//     <h3 class="title__card">${petsObj.name}</h3>
-//     <span class="typeBreed__card">${petsObj.type} - ${petsObj.breed}</span>
-//     <p class="description__card">${petsObj.description}</p>
-//     <ul class="list__card">
-//       <li class='list'>Age: <span class = "age-pets__card">${petsObj.age}</span></li>
-//       <li class='list'>Inoculations: <span class = "inoculation-pets__card">${petsObj.inoculations}</span></li>
-//       <li class="list">Diseases: <span class = "diseases-pets__card" >${petsObj.inoculations}</span></li>
-//       <li class="list"><span class = "parasites-pets__card">${petsObj.inoculations}</span></li>
-//     </ul>`
-//   popupContainer.appendChild(contentContainer)
-//   document.body.appendChild(popup)
-
-// }
-// createdCard()
-
-// console.log(content);
-
-function createdElements(tag, className, parent, text) {
-  let div = document.createElement(tag);
-  div.className = className;
-  if (text) div.textContent = text;
-  parent.appendChild(div);
-  return div;
+async function createdModalWindow(pets) {
+  let dialog = `<dialog class="dialog-window">
+      <div class="dialog-body">
+      <button class="btn__popup-close">&times</button>
+        <div class="popup-container">
+          <img
+            src=${pets.img}
+            alt=${pets.name}
+            class="img-popup" />
+          <div class="popup-content">
+            <h3 class="pet-name">${pets.name}</h3>
+            <h4 class="pet-typeBreed">${pets.type} - ${pets.breed}</h4>
+            <h5 class="pet-description">
+              ${pets.description}
+            </h5>
+            <ul class="pet-list">
+              <li class="pet-list__item">
+                <span class="list__item-text pet-age">Age:&nbsp</span
+                ><span class="pet-list__item-data">${pets.age}</span>
+              </li>
+              <li class="pet-list__item">
+                <span class="list__item-text pet-inoculations"
+                  >Inoculations:&nbsp</span
+                ><span class="pet-list__item-data">${pets.inoculations.join(
+                  " ",
+                )}</span>
+              </li>
+              <li class="pet-list__item">
+                <span class="list__item-text pet-diseases">Diseases:&nbsp</span
+                ><span class="pet-list__item-data">${pets.diseases.join(
+                  " ",
+                )}</span>
+              </li>
+              <li class="pet-list__item">
+                <span class="list__item-text pet-parasites"
+                  >Parasites:&nbsp</span
+                ><span class="pet-list__item-data">${pets.parasites}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </dialog>`;
+  return document.body.insertAdjacentHTML("afterbegin", dialog);
 }
+createdModalWindow(pets[2]);
+console.log(pets[2].inoculations.join(" "));
 
-function createdPopup(petsObj) {
-  let popup = new DocumentFragment();
-  let dialog = createdElements("dialog", "pets-dialog", popup);
-  let dialogContainer = createdElements('div', 'container__dialog',dialog)
-  let btn = createdElements("button", "button-slider", dialogContainer);
-  let container = createdElements("div", "popup-content", dialogContainer);
-  let imgPopup = createdElements("img", "img-popup", container);
-  imgPopup.src = petsObj.img;
-  imgPopup.alt = petsObj.name;
-  let content = createdElements("div", "content-popup", container);
-  for (let i = 0; i < 4; i++) {
-    switch (i) {
-      case 0:
-        createdElements("h3", "name-pert", content, petsObj.name);
-        break;
-      case 1:
-        createdElements('span', "typeBreed-pert", content, `${petsObj.type} - ${petsObj.breed}`)
-        break;
-      case 2:
-        createdElements('p', "description-pert", content, petsObj.description)
-        break;
-      case 3:
-        let listContent = createdElements('ul', 'list-pets',content);
+//********Carousel******//
 
-        let age = createdElements('li', "list-age li", listContent )
-        createdElements('span', 'text-list',age,petsObj.age)
-        let inoculations= createdElements('li', "list-inoculations li", listContent)
-        createdElements('span', 'text-list',inoculations,petsObj.inoculations.join(' '))
+const btnSliderArray = document.querySelectorAll(".button-slider");
 
-        let diseases= createdElements('li', "list-diseases li", listContent)
-        createdElements('span', 'text-list',diseases,petsObj.diseases.join(' '))
+btnSliderArray.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const click = event.target;
+    if (click.classList.contains("pets__button-left")){console.log(click);}
+  });
+});
 
-
-        let parasites=createdElements('li', "list-parasites li", listContent)
-        createdElements('span', 'text-list',parasites,petsObj.parasites.join(' '))
-
-
-
-
-    }
-  }
-
-  document.body.appendChild(popup);
+function moveCard(event) {
+  
 }
-createdPopup(pets[pets.length-1]);
-
-
-
-
