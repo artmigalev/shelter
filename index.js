4cc7d702adb5a4ddcceb561ac4fbbaaa208a5786
artmigalev <art_migalev@yahoo.com>
2025-11-30 12:36:15 +0300

    fix: path with prod and localserver

---
+++ index.js
@@ -0,0 +1,40 @@
+import { nextBtn, handle, loadedCards, prevBtn, returnedCountCards } from "./js/infinite-slider-carousel.js";
+import { createdMenuBurger, clickedBurger, BTN_BURGER } from "./js/burger.js";
+import { showModalToClickedCard,closemodal } from "./js/modal-window.js";
+
+//*____________________________________________________//
+const base = window.location.pathname.split('/')[1] === "shelter" ? "/shelter" : ".";
+  console.log(base)
+console.log(window.location)
+export const pets = await fetch(`${base}/assets/data/pets.json`)
+  .then((response) => {
+    return response.json();
+  })
+  .then(function (data) {
+    const dta = data;
+    return dta;
+  })
+  .catch((error) => console.log(error));
+
+
+if (document.readyState == "complete") createdMenuBurger();
+BTN_BURGER.addEventListener("click", clickedBurger);
+
+loadedCards(); // added pet cards to dom
+
+window.addEventListener("resize", () => {
+  document.querySelector(".block-card__slider").innerHTML = "";
+  loadedCards();
+  console.log(returnedCountCards(document.querySelector(".block-card__slider").parentNode));
+}); // changes the number of cards when changing the size of the window
+nextBtn.addEventListener("click", handle);
+prevBtn.addEventListener("click", handle);
+//* when clicked to card ---------------------------------------------------------------------------------
+document.body.querySelectorAll(".pets__card").forEach((card) => {
+  card.addEventListener("click", showModalToClickedCard);
+});
+
+
+
+
+
